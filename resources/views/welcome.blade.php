4b9eedc44b16a7c7fb68c21bbb2c6f610246a0d9
akimoto <ec2-user@ip-172-31-14-63.us-east-2.compute.internal>
2018-05-02 04:07:02 +0000

    toppage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
+    <div class="center jumbotron" style="background-color: lavender">
         <div class="text-center">
             <h1>Welcome to the ComicReview</h1>
         </div>
