44927948d3c8ebd4857253838420ac22ceeafc23
akimoto <ec2-user@ip-172-31-14-63.us-east-2.compute.internal>
2018-05-02 04:29:10 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron" style="background-color: lavender">
         <div class="text-center">
             <h1>Welcome to the ComicReview</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
